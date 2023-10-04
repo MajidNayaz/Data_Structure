aec5389909a4543e34ef2e97db365b9d1f98e915
MajidNayaz <mjnayaz1@gmail.com>
2023-10-04 11:14:41 +0430

    AVL tree complated

--- Traversal_Tree/AVL Tree/Test_Avl.java
+++ Traversal_Tree/AVL Tree/Test_Avl.java
@@ -1,8 +1,8 @@
-import java.util.Scanner;
 class Node {
     int data, height;
     Node left, right;
-    Node (int data){
+
+    Node(int data) {
         this.data = data;
         height = 1;
     }
@@ -11,63 +11,110 @@ class Node {
 public class Test_Avl {
 
     Node root;
-    int height(Node node){
-        return node==null ? 0 : node.height;
-    }
+
+    int height(Node node) {
+        return node == null ? 0 : node.height;
+       }
 
     int getBalance(Node node) {
-        return node==null ? 0 : node.left.height - node.right.height;
+        return node == null ? 0 : height(node.left) - height(node.right);
     }
 
-    public Node leftRotate(Node node) {
-
+   Node leftRotate(Node node) {
         Node rightNode = node.right;
         Node temp = rightNode.left;
 
         rightNode.left = node;
         node.right = temp;
 
+        node.height = Math.max(height(node.left), height(node.right)) + 1;
+        rightNode.height = Math.max(height(rightNode.left), height(rightNode.right)) + 1;
+
         return rightNode;
     }
 
-    public Node rightRotate(Node node) {
-
+    Node rightRotate(Node node) {
         Node leftNode = node.left;
         Node temp = leftNode.right;
 
         leftNode.right = node;
         node.left = temp;
 
+        node.height = Math.max(height(node.left),height(node.right)) + 1;
+        leftNode.height = Math.max(height(leftNode.left),height(leftNode.right)) + 1;
+
         return leftNode;
     }
 
     public Node insertNode(Node node, int data) {
-
-        if(node == null) {
-            return (new Node(data));
+        if (node == null) {
+            return new Node(data);
         }
 
-        if(data>node.data) {
-            node.right = insertNode(node.right, data);
-        }if else (data<node.data) {
+        if (node.data > data) {
             node.left = insertNode(node.left, data);
-        }else 
-        return node;
+        } else if (node.data < data) {
+            node.right = insertNode(node.right, data);
+        } else{
+            return node; 
+        }
+
+        node.height = Math.max(height(node.left),height(node.right)) + 1;
 
         int balance = getBalance(node);
 
-        if(balance > 1) {
-            if(node.left.data>data){
-                node.left = rightRotate(node.left);
+        if (balance > 1) {
+            if (node.left.data < data) {
+                node.left = leftRotate(node.left);
             }
             return rightRotate(node);
         }
 
-        if(balance < -1) {
-            if(node.right.data<data){
+        if (balance < -1) {
+            if (node.right.data > data) {
                 node.right = rightRotate(node.right);
             }
-            return rightRotate(node);
+            return leftRotate(node);
         }
+
+        return node;
+    }
+
+    void preOrder(Node node) {
+        if(node == null) {
+            return;
+        }
+        System.out.print(node.data+", ");
+        preOrder(node.left);
+        preOrder(node.right);
+    }
+
+    void inOrder (Node node) {
+        if(node == null) {
+            return;
+        }        
+        inOrder(node.left);
+        System.out.print(node.data+", ");
+        inOrder(node.right);
+    }
+
+    public static void main(String[] args) {
+
+        Test_Avl av = new Test_Avl();
+        av.root = av.insertNode(av.root, 67);
+        av.root = av.insertNode(av.root, 34);
+        av.root = av.insertNode(av.root, 68);
+        av.root = av.insertNode(av.root, 70);
+        av.root = av.insertNode(av.root, 100);
+        av.root = av.insertNode(av.root, 20);
+        av.root = av.insertNode(av.root, 15);
+        av.root = av.insertNode(av.root, 10);
+        av.insertNode(av.root, 39);
+        System.out.println("preOrder root left right: ");
+        av.preOrder(av.root);
+        System.out.println();
+        System.out.println("inOrder left root right");
+        av.inOrder(av.root);
+        System.out.println();
     }
 }
