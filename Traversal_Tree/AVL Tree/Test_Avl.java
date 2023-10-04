64ff038ce4af4d8bd9d55c0135b9f00f296b9311
MajidNayaz <mjnayaz1@gmail.com>
2023-10-04 08:17:49 +0430

    height and getblance added to avl tree

--- Traversal_Tree/AVL Tree/Test_Avl.java
+++ Traversal_Tree/AVL Tree/Test_Avl.java
@@ -1,9 +1,29 @@
-
+import java.util.Scanner;
 class Node {
     int data, height;
     Node left, right;
     Node (int data){
         this.data = data;
         height = 1;
+    }
+}
+/**
+ * Test_Avl
+ */
+public class Test_Avl {
+
+    Node root;
+    int height(Node node){
+        if(node == null) {
+            return 0;
+        }
+    return node.height;
+    }
+
+    int getBalance(Node node) {
+        if(node == null) {
+            return 0;
+        }
+        return node.left.height - node.right.height;
     }
 }
