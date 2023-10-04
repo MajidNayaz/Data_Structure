bacaac82c145b98ff4e64002ae60d28877f2a3c7
MajidNayaz <mjnayaz1@gmail.com>
2023-10-04 08:35:45 +0430

    left and right ratation added

--- Traversal_Tree/AVL Tree/Test_Avl.java
+++ Traversal_Tree/AVL Tree/Test_Avl.java
@@ -14,16 +14,44 @@ public class Test_Avl {
 
     Node root;
     int height(Node node){
-        if(node == null) {
-            return 0;
-        }
-    return node.height;
+        return node==null ? 0 : node.height;
     }
 
     int getBalance(Node node) {
+        return node==null ? 0 : node.left.height - node.right.height;
+    }
+
+    public Node leftRotate(Node node) {
+
+        Node rightNode = node.right;
+        Node temp = rightNode.left;
+
+        rightNode.left = node;
+        node.right = temp;
+
+        return rightNode;
+    }
+
+    public Node rightRotate(Node node) {
+
+        Node leftNode = node.left;
+        Node temp = leftNode.right;
+
+        leftNode.right = node;
+        node.left = temp;
+
+        return leftNode;
+    }
+
+    public Node insertNode(Node node, int data) {
+
         if(node == null) {
-            return 0;
+            node = new Node(data);
+            return node;
         }
-        return node.left.height - node.right.height;
+
+        
+
+
     }
 }
